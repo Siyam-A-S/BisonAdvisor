85f283ac9d4fbc983531fa5bbab9f7b6b2ee555d
Siyam Al Shahriar <98270965+Siyam-A-S@users.noreply.github.com>
2023-12-11 16:32:28 -0500

    Update About us.py

--- pages/About us.py
+++ pages/About us.py
@@ -1,1 +1,4 @@
+import streamlit as st
 
+st.title("About us)
+st.button(Click on me, key=None, help=None, on_click=None, args=None, kwargs=None, *, type="secondary", disabled=False, use_container_width=False)
