47eaeec4038e0b51c81ed65c55fde9a02e8b66ad
Siyam Al Shahriar <98270965+Siyam-A-S@users.noreply.github.com>
2023-12-11 16:34:33 -0500

    Update About us.py

--- pages/About us.py
+++ pages/About us.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
 st.title("About us)
-st.button("Click on me", key=None, help=None, on_click=None, args=None, kwargs=None, *, type="secondary", disabled=False, use_container_width=False)
+
+
+st.button("Reset", type="primary")
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
