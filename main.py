d4f41a17ddf2b72144a38be4cacb55a9e8ed7cb0
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-12-12 14:29:55 -0500

    a23

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main_app():
     with st.sidebar:
         selected = option_menu("Main Menu",
                                ["My Profile", 'ChatBot', 'Registration Form Generator', 'Checklist',
-                                'Self Service Resources'],
+                                'Resources'],
                                icons=['file-person', 'chat-dots', 'file-earmark-text', 'card-checklist', 'info-circle'],
                                menu_icon="cast",
                                default_index=0)
